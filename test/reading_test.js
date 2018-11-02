267f8793dc7a531d9237b6d297190bdd7676157f
Hassaan-Ahmed <dashingsd@gmail.com>
2018-11-02 13:09:15 +0500

    reading user from database is been completed

--- test/reading_test.js
+++ test/reading_test.js
@@ -1,3 +1,28 @@
 const assert = require('assert');
 const User = require('../src/user');
 
+describe('Reading user from database with the name joe',() => {
+    let joe;
+    beforeEach((done)=>{
+       joe = new User({name: 'joe'});
+       joe.save()
+           .then(() => done());
+    });
+
+    it('find user with the name of joe', (done) => {
+        User.find({name: 'joe'})
+            .then((users)=>{
+               assert(users[0]._id.toString() === joe._id.toString());
+               done();
+            });
+    });
+
+    it('should find the user with the same id', (done) => {
+        User.findOne({_id: joe._id})
+            .then((user) => {
+                assert(user.name === 'joe');
+                done();
+            })
+
+    });
+});
